7c5e0c43ce3cc994f9f2cee27fcce4da3994d99c
karanmeghani18 <karanmeghani18@gmail.com>
2023-03-24 19:03:54 -0400

    Initial Commit

---
+++ finalProjectG11/finalProjectG11App.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectG11App.swift
+//  finalProjectG11
+//
+//  Created by Karan Meghani on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectG11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
